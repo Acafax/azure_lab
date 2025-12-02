a3b27438c364ac3faa9617bdf1c188774b5aecb4
Wiktor <acafax>
2025-12-02 17:28:24 +0100

    fix9

--- skrypt.sh
+++ skrypt.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 # A. Instalacja (wyciszona)
-echo '>>> [1/5] Aktualizacja i instalacja LVM...'
+echo '>>> [1/7] Aktualizacja i instalacja LVM...'
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get install -y lvm2 > /dev/null 2>&1
 
 # B. Inicjalizacja Dysków Fizycznych (PV)
-echo '>>> [2/5] Tworzenie Physical Volumes...'
-sudo pvcreate /dev/sdc /dev/sdd /dev/sde
+# -ff -y aby nie było błędu jezeli zostaną stare partycje
+echo '>>> [2/7] Tworzenie Physical Volumes...'
+sudo pvcreate -ff -y /dev/sdc /dev/sdd /dev/sde > /dev/null
 
 # C. Tworzenie Grupy (VG)
-echo '>>> [3/5] Tworzenie Volume Group (data_vg)...'
-sudo vgcreate data_vg /dev/sdc /dev/sdd /dev/sde
+echo '>>> [3/7] Tworzenie Volume Group (data_vg)...'
+sudo vgcreate data_vg /dev/sdc /dev/sdd /dev/sde > /dev/null
 
 # D. Tworzenie Woluminów Logicznych (LV) - RÓWNOLEGLE
-echo '>>> [4/5] Tworzenie LV: Linear, Striped, Mirror...'
+echo '>>> [4/7] Tworzenie LV: Linear, Striped, Mirror...'
 
 # 1. LINEAR (5GB) - bierze po kolei
+echo '>>> [5/7] Tworzenie LV: Linear, Striped, Mirror...'
 sudo lvcreate -n lv_linear -L 5G data_vg > /dev/null
 
 # 2. STRIPED (6GB) - paski na 3 dyskach (stąd -i 3)
+echo '>>> [6/7] Tworzenie LV: Linear, Striped, Mirror...'
 sudo lvcreate -n lv_striped -L 6G -i 3 data_vg > /dev/null
 
-# 3. MIRROR (2GB danych) - kopia 1:1 (wymaga 4GB miejsca fizycznego, log w pamięci --mirrorlog core dla prostoty)
+# 3. MIRROR (2GB danych) - kopia 1:1 (wymaga 4GB miejsca fizycznego)
+echo '>>> [7/7] Tworzenie LV: Mirror'
 sudo lvcreate --type raid1 -n lv_mirror -L 2G -m 1 data_vg > /dev/null
 
 # E. Formatowanie i Montowanie
